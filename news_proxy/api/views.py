2f8c6c0928cf268a7669d8072659f24180160982
Aqsa Khalid <aqsakhalid1014@gmail.com>
2023-12-08 17:24:59 +0500

    added view for news api and addde sample env

--- news_proxy/api/views.py
+++ news_proxy/api/views.py
@@ -2,3 +2,19 @@ from django.http import JsonResponse
 import requests
 from decouple import config
 
+def news_proxy(request):
+    try:
+        api_key = config('NEWS_API_KEY')
+        api_url = config('NEWS_API_URL')
+        query = request.GET.get('q', 'apple')
+        language = request.GET.get('language', 'en')
+        api_url = f'{api_url}?q={query}&from=past7days&sortBy=publishedAt&language={language}'
+        
+        response = requests.get(api_url, headers={'Authorization': f'Bearer {api_key}'})
+        response.raise_for_status()
+        return JsonResponse(response.json())
+
+    except requests.HTTPError as http_err:
+        return JsonResponse({'error': f'HTTP error occurred: {http_err}'}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': f'An error occurred: {e}'}, status=503)
